e5e3a0781e8f4431957ef28ff50c775fa77a2bac
parkseunghan <01seunghan@gmail.com>
2025-05-16 14:48:39 +0900

    config.php 추적 제거

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config/config.php
-$mysqli = new mysqli("localhost", "guest", "guest", "board");
-if ($mysqli->connect_error) {
-    die("DB 연결 실패: " . $mysqli->connect_error);
-}
-session_start();
-?>
